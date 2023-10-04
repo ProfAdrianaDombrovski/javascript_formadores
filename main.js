a5f3185eeb3083f3f99ba2cd3a75957e42444c0d
ADRIANA DOMBROVSKI <100074324+ProfAdrianaDombrovski@users.noreply.github.com>
2023-10-04 10:29:47 -0300

    som grilo

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ function tocaSomRisada()
     document.querySelector('#som_tecla_risada').play();
 }
 document.querySelector('.tecla_risada') .onclick = tocaSomRisada;
+
+
+function tocaSomGrilo()
+{
+    document.querySelector('#som_tecla_grilo').play();
+}
+document.querySelector('.tecla_grilo') .onclick = tocaSomGrilo;
