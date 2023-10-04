71414f5a26757fd4d743d02e41790cc3444a23d4
ADRIANA DOMBROVSKI <100074324+ProfAdrianaDombrovski@users.noreply.github.com>
2023-10-04 09:26:35 -0300

    ok

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ function tocaSomRisada()
 {
     document.querySelector('#som_tecla_risada').play();
 }
-document.querySelector('.tecla_risada') .onclick=tocaSomRisada;
+document.querySelector('.tecla_risada') .onclick = tocaSomRisada;
