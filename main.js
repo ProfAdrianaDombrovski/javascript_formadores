9e10a5025003baa5a7e237760f120e5cb88f15f8
ADRIANA DOMBROVSKI <100074324+ProfAdrianaDombrovski@users.noreply.github.com>
2023-10-05 13:39:38 -0300

    ajustes

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ function tocaSomRisada()
     document.querySelector('#som_tecla_risada').play();
 }
 
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomRisada;
 
