2965b1de1fcf8b9b0de4a124ac1ecbf8fb0631fb
ADRIANA DOMBROVSKI <100074324+ProfAdrianaDombrovski@users.noreply.github.com>
2023-10-05 13:30:30 -0300

    query

--- main.js
+++ main.js
@@ -3,18 +3,7 @@ function tocaSomRisada()
 {
     document.querySelector('#som_tecla_risada').play();
 }
-document.querySelector('.tecla_risada') .onclick = tocaSomRisada;
 
 
-function tocaSomGrilo()
-{
-    document.querySelector('#som_tecla_grilo').play();
-}
-document.querySelector('.tecla_grilo') .onclick = tocaSomGrilo;
+document.querySelectorAll('.tecla')
 
-
-function tocaSomVaia()
-{
-    document.querySelector('#som_tecla_vaia').play();
-}
-document.querySelector('.tecla_vaia') .onclick = tocaSomVaia;
