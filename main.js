d492775109c84a65b8eaefe0bcf2ce81476bc9fd
ADRIANA DOMBROVSKI <100074324+ProfAdrianaDombrovski@users.noreply.github.com>
2023-11-28 19:13:41 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-    
+    const elemento = document.querySelector(idElementoAudio);
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log("Elemento não encontrado ou seletor inválido")
+    }
 }
 const listadeTeclas = document.querySelectorAll(".tecla");
 
